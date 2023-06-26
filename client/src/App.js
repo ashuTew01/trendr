031641472b3fa75654e0c1b02080bed6bd1635de
Ashutosh Tewari <krishwave66@gmail.com>
2023-06-27 01:02:50 +0530

    fix navigation in react router

--- client/src/App.js
+++ client/src/App.js
@@ -24,8 +24,9 @@ function App() {
         <ThemeProvider theme={theme}>
         <CssBaseline />   {/* reset css for material ui */}
           <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" /> } />
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/login" element={isAuth ? <Navigate to="/home" /> : <LoginPage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/login" /> } />
             <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" /> } />
           </Routes>
         </ThemeProvider>
